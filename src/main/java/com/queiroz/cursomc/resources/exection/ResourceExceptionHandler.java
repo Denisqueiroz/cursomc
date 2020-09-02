23eed0a66b1583fb0c7927870e05e68051b75586
Denis Queiroz <denisqueiroz1@gmail.com>
2020-09-02 18:58:07 -0300

    Listando Todas as Categorias com TDO
    Listando Todas as Categorias com TDO

--- src/main/java/com/queiroz/cursomc/resources/exection/ResourceExceptionHandler.java
+++ src/main/java/com/queiroz/cursomc/resources/exection/ResourceExceptionHandler.java
@@ -2,20 +2,31 @@ package com.queiroz.cursomc.resources.exection;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.queiroz.cursomc.services.exception.DataIntegrityException;
+import com.queiroz.cursomc.services.exception.ObjectNotFoundException;
+
+
 @ControllerAdvice
 public class ResourceExceptionHandler {
-
+	
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objResponseEntity(ObjectNotFoundException e, HttpServletRequest request) {
-
-		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Integridade de dados", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	
 }
--- src/main/java/com/queiroz/cursomc/services/CategoriaService.java
+++ src/main/java/com/queiroz/cursomc/services/CategoriaService.java
@@ -1,12 +1,15 @@
 package com.queiroz.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.queiroz.cursomc.domain.Categoria;
 import com.queiroz.cursomc.repositories.CategoriaRepository;
+import com.queiroz.cursomc.services.exception.DataIntegrityException;
 import com.queiroz.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -20,4 +23,30 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+
 }
