92898ac244d1cd86c8df336853f31690c7055eea
Denis Queiroz <denisqueiroz1@gmail.com>
2020-09-04 10:24:13 -0300

    Validação sintatica com bean validation
    Validação sintatica com bean validation

--- src/main/java/com/queiroz/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/queiroz/cursomc/dto/CategoriaDTO.java
@@ -4,14 +4,19 @@ package com.queiroz.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.queiroz.cursomc.domain.Categoria;
-
-
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
@@ -38,4 +43,3 @@ public class CategoriaDTO implements Serializable {
 		this.nome = nome;
 	}	
 }
-
--- src/main/java/com/queiroz/cursomc/services/CategoriaService.java
+++ src/main/java/com/queiroz/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.queiroz.cursomc.domain.Categoria;
+import com.queiroz.cursomc.dto.CategoriaDTO;
 import com.queiroz.cursomc.repositories.CategoriaRepository;
 import com.queiroz.cursomc.services.exception.DataIntegrityException;
 import com.queiroz.cursomc.services.exception.ObjectNotFoundException;
@@ -56,5 +57,11 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
+	
+   public Categoria fromDTO(CategoriaDTO objDto) {
+	   
+	   return new Categoria(objDto.getId(), objDto.getNome());
+	   
+   }
 
 }
