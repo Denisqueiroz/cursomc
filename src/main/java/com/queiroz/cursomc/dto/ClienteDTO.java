79a8e775fee6ddd0625bf8c04d0c4eef7716f21f
Denis Queiroz <denisqueiroz1@gmail.com>
2020-10-20 09:26:31 -0300

    Busca de Pedidos por nome e categorias
    Busca de Pedidos por nome e categorias

--- src/main/java/com/queiroz/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/queiroz/cursomc/dto/ClienteDTO.java
@@ -15,11 +15,11 @@ public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
-	
-	@NotEmpty(message="Preenchimento obrigatório")
-	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 80 caracteres")
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
-	@NotEmpty(message="Preenchimento obrigatório")
+	@NotEmpty(message = "Preenchimento obrigatório")
 	@Email(message = "Email Invalido")
 	private String email;
 
